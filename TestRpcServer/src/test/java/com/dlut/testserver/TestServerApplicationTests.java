10eb22a8f611583930ed95f155e958428ea8f6da
NY-WaKeUp <785539058@qq.com>
2025-04-21 11:57:14 +0800

    rpc_by_hand
    (cherry picked from commit e3e3ad27d5dc786fe0ce1ed610e105d78fc9396f)

---
+++ TestRpcServer/src/test/java/com/dlut/testserver/TestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlut.testserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
